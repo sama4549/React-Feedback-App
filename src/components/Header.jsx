d983cda5b35ee0c2ca704c67c34a023b1347d784
sanderson <anderson.samuel61@gmail.com>
2022-01-27 16:44:35 -0600

    saved to allow work at home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,30 @@
-function Header() {
-  return <div></div>;
+import PropTypes from 'prop-types';
+
+function Header({ text, bgColor, textColor }) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  }
+
+  return (
+    <header style={headerStyles}>
+      <div className="container">
+        <h2>{text}</h2>
+      </div>
+    </header>
+  );
+}
+
+Header.defaultProps = {
+  text: 'Feedback UI',
+  bgColor: 'rgba(0,0,0,0.4)',
+  textColor: '#ff6a95',
+}
+
+Header.propTypes = {
+  test: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
 }
 
 export default Header;
