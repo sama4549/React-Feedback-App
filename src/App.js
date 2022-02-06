1e590d4a24929c2cbcd4e37839010ccc74744bc9
Sam Anderson <anderson.samuel61@gmail.com>
2022-02-06 11:20:36 -0600

    created functions from app.js using state

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
+import FeedbackStats from './components/FeedbackStats';
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
+
+    const deleteList = (id) => {
+        if(window.confirm('Are you sure you would like to delete this item?')) {
+            setFeedback(feedback.filter((item) => {
+                return item.id !== id
+            }))
+        }
+    }
 
     return (
         <>
             <Header />
             <div className="container">
-                <FeedbackList feedback={feedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList feedback={feedback} handleList={deleteList} />
             </div>
         </>
     )
---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function FeedbackStats({ feedback }) {
+  return <div className='feedback-stats'>
+      <h4>{feedback.length} Reviews</h4>
+  </div>;
+}
+
+export default FeedbackStats;
