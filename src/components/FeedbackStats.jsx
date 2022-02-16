a1309a121ab25f423f6411489e8d52d2815ef4fc
Sam Anderson <anderson.samuel61@gmail.com>
2022-02-15 20:26:58 -0600

    further small updates

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 function FeedbackStats({ feedback }) {
-  return <div className='feedback-stats'>
+
+  let average = feedback.reduce((acc, cur) => {
+    return acc + cur.rating;
+  }, 0) / feedback.length
+
+  average = average.toFixed(1).replace(/[.,]0$/,'')
+
+  return (<div className='feedback-stats'>
       <h4>{feedback.length} Reviews</h4>
-  </div>;
+      <h4>Average Score: {isNaN(average) ? 0 : average}</h4>
+  </div>
+  )
+}
+
+FeedbackStats.propTypes = {
+  feedback: PropTypes.array.isRequired,
 }
 
 export default FeedbackStats;
